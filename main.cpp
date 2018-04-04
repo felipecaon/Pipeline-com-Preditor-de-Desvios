31279540e8c0f5488b630b0460028b862a2146e9
Felipe Caon <felipecaon@gmail.com>
2018-04-04 17:30:13 -0300

    Pipeline comentada

--- main.cpp
+++ main.cpp
@@ -3,23 +3,34 @@
 
 //variaveis globais
 int PC = 1;
+int contInstr = 0;
+bool flag = false;
 
 int main() {
 
     LeituraASM arquivo("teste2.txt");
 
     while (!arquivo.fetch(PC)) {
+        flag = false;
         arquivo.decode(arquivo);
         arquivo.execute();
+        if(arquivo.passo_execute.opCode == "j"){
+            PC = PC + arquivo.getRAuxiliares()[0];
+            flag = true;
+        }
         arquivo.memoria();
         arquivo.WriteBack();
-        PC++;
+        if(flag == false){
+            PC++;
+        }
+
+        contInstr++;
     }
 
-
-    cout << arquivo.getR()[2] << endl << endl;
-    cout << arquivo.getR()[6] << endl << endl;
-    cout << arquivo.getR()[12] << endl << endl;
-    cout << arquivo.getR()[15] << endl << endl;
+    cout << "Instrucoes executadas: " << contInstr << endl;
+    cout << arquivo.getR()[2] << endl;
+    cout << arquivo.getR()[6] << endl;
+    cout << arquivo.getR()[12] << endl;
+    cout << arquivo.getR()[15] << endl;
 
 }
